6b3742acfcfc40c55af2ab40540684b62f564469
Alberto Fabian <lf_alberto@hotmail.com>
2020-01-29 17:47:57 -0600

    Primer commit, vistas principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { DasboardComponent } from './pages/dasboard/dasboard.component';
+import { RouterModule, Routes} from '@angular/router';
+import { InformesComponent } from './pages/informes/informes.component';
+import { AjustesComponent } from './pages/ajustes/ajustes.component';
+import { TicketsComponent } from './pages/tickets/tickets.component';
+
+
+// Aquí se genera el entutamiento para las paginas
+const routes: Routes = [
+  {path:'',redirectTo:'/dashboard', pathMatch:'full' },
+  {path:'dashboard',component:DasboardComponent},
+  {path:'informes',component:InformesComponent},
+  {path:'ajustes',component:AjustesComponent},
+  {path:'tickets',component:TicketsComponent}
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    DasboardComponent,
+    InformesComponent,
+    AjustesComponent,
+    TicketsComponent,
+
+    
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //Imortante 
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+
+
+}
